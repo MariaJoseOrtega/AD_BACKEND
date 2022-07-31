4268c835500272b85d8ba0bbee403dc3b1fbf9e8
Didyer <dao.gamboa@yavirac.edu.ec>
2022-07-30 19:10:04 -0500

    add authority

--- src/main/java/yavirac/seguridad/feature/permiso/Permiso.java
+++ src/main/java/yavirac/seguridad/feature/permiso/Permiso.java
@@ -1,9 +1,12 @@
 package yavirac.seguridad.feature.permiso;
 
 import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
@@ -23,6 +26,7 @@ public class Permiso {
     private boolean enabled;
     private Long personId;
     
-    
+    @MappedCollection(idColumn = "permiso_id")
+    private Set<PermisoAuthority> authorities = new HashSet<>();
     
 }
